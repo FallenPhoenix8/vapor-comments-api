4f234f5ca72b6673c8fe12184c39ac45512acf01
FallenPhoenix8 <tplayxyz8@gmail.com>
2025-01-31 21:07:13 +0100

    Update WebSocketManager.swift - add support for multiple instances

--- Sources/App/WebSocketManager.swift
+++ Sources/App/WebSocketManager.swift
@@ -1,48 +1,32 @@
 import Vapor
 
-// enum WebSocketManager {
-//     static func addConnection(_ ws: WebSocket) {
-//         ws.onClose.whenComplete { _ in
-//             removeConnection(ws)
-//         }
-
-//         GlobalStorage.wsConnections.append(ws)
-//     }
-
-//     private static func removeConnection(_ ws: WebSocket) {
-//         GlobalStorage.wsConnections = GlobalStorage.wsConnections.filter { $0 !== ws }
-//     }
-
-//     static func broadcast(_ message: Data) {
-//         for ws in GlobalStorage.wsConnections {
-//             ws.send(message)
-//         }
-//     }
-// }
-
 final class WebSocketManager: @unchecked Sendable {
     nonisolated(unsafe) static var wsConnections: [WebSocket] = []
-    static let queue = DispatchQueue(label: "wsConnections")
 
-    static func addConnection(_ ws: WebSocket) {
+    let queue: DispatchQueue
+    init(threadLabel: String) {
+        queue = .init(label: threadLabel)
+    }
+
+    func addConnection(_ ws: WebSocket) {
         ws.onClose.whenComplete { _ in
             self.removeConnection(ws)
         }
 
         queue.sync {
-            self.wsConnections.append(ws)
+            type(of: self).wsConnections.append(ws)
         }
     }
 
-    private static func removeConnection(_ ws: WebSocket) {
+    private func removeConnection(_ ws: WebSocket) {
         queue.sync {
-            self.wsConnections = self.wsConnections.filter { $0 !== ws }
+            type(of: self).wsConnections = type(of: self).wsConnections.filter { $0 !== ws }
         }
     }
 
-    static func broadcast(_ message: Data) {
+    func broadcast(_ message: Data) {
         queue.sync {
-            for ws in self.wsConnections {
+            for ws in type(of: self).wsConnections {
                 ws.send(message)
             }
         }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,7 @@
 import Vapor
 
 let fileManager: FileManager = .init()
+let wsManagerComments: WebSocketManager = .init(threadLabel: "wsManagerComments")
 
 func routes(_ app: Application) throws {
     app.get { req async -> Response in
@@ -20,7 +21,7 @@ func routes(_ app: Application) throws {
         let id = try req.query.get(Double.self, at: "id")
         let comments = try await Comment.deleteComment(id: id)
 
-        try WebSocketManager.broadcast(JSONEncoder().encode(comments))
+        try wsManagerComments.broadcast(JSONEncoder().encode(comments))
 
         return req.fileio.streamFile(at: "Sources/comments.json")
     }
@@ -31,12 +32,12 @@ func routes(_ app: Application) throws {
         let comment = Comment(content: content)
         let comments = try await comment.addComment()
 
-        try WebSocketManager.broadcast(JSONEncoder().encode(comments))
+        try wsManagerComments.broadcast(JSONEncoder().encode(comments))
 
         return req.fileio.streamFile(at: "Sources/comments.json")
     }
 
     app.webSocket("ws") { _, ws in
-        WebSocketManager.addConnection(ws)
+        wsManagerComments.addConnection(ws)
     }
 }
