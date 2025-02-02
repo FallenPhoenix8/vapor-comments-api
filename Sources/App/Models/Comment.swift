4de09f7a73ea3abcbca5702482dcf164539b83cd
FallenPhoenix8 <tplayxyz8@gmail.com>
2025-02-02 18:07:10 +0100

    Add documentation to `WebsocketManager.swift` and `Comment.swift`

--- Sources/App/Models/Comment.swift
+++ Sources/App/Models/Comment.swift
@@ -1,8 +1,27 @@
 import Foundation
 import Vapor
 
+/// A model representing a comment with a unique identifier and content.
+///
+/// The `Comment` struct is used to encapsulate the details of a comment, including its content and a unique identifier.
+/// Comments are stored in a global static array and can be added or removed. This struct also supports encoding and decoding
+/// to facilitate easy storage and retrieval from a JSON file.
+///
+/// Fields:
+/// - **`id`**: A unique `Double` identifier for the comment, generated based on the current time.
+/// - **`content`**: The textual content of the comment.
+///
+/// Methods:
+/// - **`init(content: String)`**: Initializes a new comment with the given content and a unique ID.
+/// - **`init(id: Double) throws`**: Initializes a comment by its ID, throwing an error if not found.
+/// - **`add() async throws -> [Comment]`**: Adds the comment to storage and returns the updated list of comments.
+/// - **`delete() async throws -> [Comment]`**: Deletes the comment from storage and returns the updated list of comments.
+/// - **`updateCommentGlobalStorage() -> [Comment]`**: Updates the global storage of comments from the JSON file.
+
 public struct Comment: Decodable, Encodable, Sendable, Identifiable {
-    public nonisolated(unsafe) static var comments: [Comment] = []
+    /// The global storage of comments. This array is used to store and retrieve comments from storage.
+    private nonisolated(unsafe) static var comments: [Comment] = []
+
     private static let queue = DispatchQueue(label: "comments")
 
     public let id: Double
@@ -13,6 +32,11 @@ public struct Comment: Decodable, Encodable, Sendable, Identifiable {
         self.content = content
     }
 
+    /// Initializes a comment by its ID, throwing an error if not found.
+    ///
+    /// Use this initializer to fetch a comment from storage by its ID. If the comment is not found, an error will be thrown.
+    /// - Parameter id: The unique identifier of the comment to fetch.
+    /// - Throws: An error if the comment cannot be found.
     init(id: Double) throws {
         self.id = id
         var existingComment: Comment? = nil
@@ -21,11 +45,20 @@ public struct Comment: Decodable, Encodable, Sendable, Identifiable {
                 existingComment = comment
             }
         }
-        content = existingComment?.content ?? ""
+
         if existingComment == nil {
             throw Abort(.notFound)
         }
+        content = existingComment?.content ?? ""
     }
+
+    /// Updates the global storage of comments from the JSON file.
+    ///
+    /// This method reads the saved comments from the "Sources/comments.json" file and updates the static `comments` array with the retrieved data.
+    /// If the file does not exist, it creates an empty JSON file to store comments.
+    ///
+    /// - Returns: An array of `Comment` objects representing the current global storage of comments.
+    /// - Aborts request: Aborts connection if there is an issue decoding the JSON file.
 
     static func updateCommentGlobalStorage() -> [Comment] {
         if !fileManager.fileExists(atPath: "Sources/comments.json") {
@@ -44,7 +77,14 @@ public struct Comment: Decodable, Encodable, Sendable, Identifiable {
         return comments
     }
 
-    private static func writeComments(comments: [Comment]) async -> Data {
+    /// Writes the given comments to the "Sources/comments.json" file.
+    ///
+    /// This method writes the given array of `Comment` objects to the JSON file, overwriting any existing file contents.
+    /// - Parameter comments: The array of `Comment` objects to write to storage.
+    /// - Returns: The JSON data representing the stored comments.
+    /// - Aborts request: Aborts connection if there is an issue encoding or writing the JSON file.
+
+    private static func writeComments(comments: [Comment]) -> Data {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
         var data = Data()
@@ -63,21 +103,37 @@ public struct Comment: Decodable, Encodable, Sendable, Identifiable {
         return data
     }
 
+    /// Adds the current comment to the global storage.
+    ///
+    /// This asynchronous method appends the current `Comment` instance to the global storage array of comments,
+    /// then updates the JSON file with the new list of comments.
+    ///
+    /// - Returns: An updated array of `Comment` objects representing the current global storage.
+    /// - Throws: An error if the process of adding the comment or updating the storage fails.
+
     func add() async throws -> [Comment] {
         var comments = type(of: self).updateCommentGlobalStorage()
         comments.append(self)
 
-        _ = await type(of: self).writeComments(comments: comments)
+        _ = type(of: self).writeComments(comments: comments)
         _ = Comment.updateCommentGlobalStorage()
 
         return comments
     }
 
+    /// Deletes the current comment from the global storage.
+    ///
+    /// This asynchronous method removes the `Comment` instance with the matching ID from the global storage array,
+    /// then updates the JSON file to reflect the removal.
+    ///
+    /// - Returns: An updated array of `Comment` objects representing the current global storage after the deletion.
+    /// - Throws: An error if the process of deleting the comment or updating the storage fails.
+
     func delete() async throws -> [Comment] {
         let id = self.id
         var comments = type(of: self).updateCommentGlobalStorage()
         comments = comments.filter { $0.id != id }
-        _ = await type(of: self).writeComments(comments: comments)
+        _ = type(of: self).writeComments(comments: comments)
         _ = Comment.updateCommentGlobalStorage()
         return comments
     }
--- Sources/App/WebSocketManager.swift
+++ Sources/App/WebSocketManager.swift
@@ -1,14 +1,30 @@
 import Vapor
 
+/// Manages multiple connections to a WebSocket, allowing for messages to be broadcast to all connected clients.
+///
+/// This class is thread-safe and can be used to manage connections to a single WebSocket from multiple threads.
+///
+/// To use this class:
+///
+/// 1. Create an instance of `WebSocketManager`.
+/// 2. When a new WebSocket connection is made, call `addConnection(_:)` with the new WebSocket.
+/// 3. When a message is sent to the WebSocket, call `broadcast(_:)` with the message.
 final class WebSocketManager: @unchecked Sendable {
-    nonisolated(unsafe) static var wsConnections: [WebSocket] = []
+    /// The array of WebSocket connections managed by this manager.
+    private nonisolated(unsafe) static var wsConnections: [WebSocket] = []
 
-    let queue: DispatchQueue
+    /// The dispatch queue used to synchronize access to the `wsConnections` array.
+    private let queue: DispatchQueue
+
+    /// Initializes a new instance of `WebSocketManager`.
+    /// - Parameter threadLabel: This is the label used to create the dispatch queue. It is used to identify the queue.
     init(threadLabel: String) {
         queue = .init(label: threadLabel)
     }
 
-    func addConnection(_ ws: WebSocket) {
+    /// Adds a new WebSocket connection to the manager.
+    /// - Parameter ws: The WebSocket connection to add.
+    public func addConnection(_ ws: WebSocket) {
         ws.onClose.whenComplete { _ in
             self.removeConnection(ws)
         }
@@ -18,13 +34,17 @@ final class WebSocketManager: @unchecked Sendable {
         }
     }
 
+    /// Removes a WebSocket connection from the manager.
+    /// - Parameter ws: The WebSocket connection to remove.
     private func removeConnection(_ ws: WebSocket) {
         queue.sync {
             type(of: self).wsConnections = type(of: self).wsConnections.filter { $0 !== ws }
         }
     }
 
-    func broadcast(_ message: Data) {
+    /// Broadcasts a message to all connected WebSocket clients.
+    /// - Parameter message: The message to broadcast.
+    public func broadcast(_ message: Data) {
         queue.sync {
             for ws in type(of: self).wsConnections {
                 ws.send(message)
